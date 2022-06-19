c120aa4e03d399d40030758c16d7580eac37fb26
이진우 <leejinwoo1126@gmail.com>
2022-06-19 14:02:56 +0900

    step13. ItemBusinessServiceTest 기본 작성

--- src/main/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessService.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessService.java
@@ -4,16 +4,19 @@ import com.in28minutes.unittesting.unittesting.data.ItemRepository;
 import com.in28minutes.unittesting.unittesting.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Component
 public class ItemBusinessService {
 	
 	@Autowired
 	private ItemRepository repository;
-	
+
+	public ItemBusinessService(ItemRepository repository) {
+		this.repository = repository;
+	}
+
 	public Item retreiveHardcodedItem() {
 		return new Item(1, "Ball", 10, 100);
 	}
---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessServiceTest.java
@@ -0,0 +1,41 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
+import com.in28minutes.unittesting.unittesting.model.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+//@RunWith(MockitoJUnitRunner.class) 요거 하니 실행 안됨
+@ExtendWith(MockitoExtension.class)
+class ItemBusinessServiceTest {
+
+    @InjectMocks
+    private ItemBusinessService itemBusinessService;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @Test
+    public void calculateSumUsingDataService_basic() {
+        when(itemRepository.findAll())
+                .thenReturn(Arrays.asList(new Item(10001, "Item1", 10, 20), new Item(10002, "Item2", 5, 10)));
+
+        List<Item> items = itemBusinessService.retrieveAllItems();
+
+        assertEquals(200, items.get(0).getValue());
+        assertEquals(50, items.get(1).getValue());
+    }
+
+}
